dc1529c8231d997a8476cd1aa79974c98c1de844
saracristinas <sarasales17062000@gmail.com>
2026-03-22 17:23:58 -0300

    feat(repository): criação do repositório de usuários com métodos de email

--- src/main/java/com/br/usermanager/user/repositories/UserRepository.java
+++ src/main/java/com/br/usermanager/user/repositories/UserRepository.java
@@ -8,4 +8,8 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
