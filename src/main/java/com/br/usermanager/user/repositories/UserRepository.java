26224ceb0269cb5724bfceb0beb0b50bb3afcae2
saracristinas <sarasales17062000@gmail.com>
2026-03-23 02:45:49 -0300

    feat: adiciona busca por email no repository

--- src/main/java/com/br/usermanager/user/repositories/UserRepository.java
+++ src/main/java/com/br/usermanager/user/repositories/UserRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
