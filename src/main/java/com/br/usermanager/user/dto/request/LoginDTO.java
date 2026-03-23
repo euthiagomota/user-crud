9a7bfa61670411ae55453d17047f47df9276425c
saracristinas <sarasales17062000@gmail.com>
2026-03-23 02:43:35 -0300

    feat: adiciona configuração global de CORS

---
+++ src/main/java/com/br/usermanager/user/dto/request/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.br.usermanager.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginDTO(
+        @NotBlank(message = "Email e obrigatorio")
+        @Email(message = "Email invalido")
+        String email,
+
+        @NotBlank(message = "Senha e obrigatoria")
+        String senha
+) {
+}
+
